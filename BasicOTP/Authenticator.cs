cb727e29ee8fd090af85bdc2b53e6bb0db98cc0f
Jason Davis <jasondavis303@gmail.com>
2021-04-21 20:27:01 -0600

    Add GetRemainingTime, add public function descriptions, code cleanup, optimize timing math

--- BasicOTP/Authenticator.cs
+++ BasicOTP/Authenticator.cs
@@ -6,16 +6,21 @@ namespace BasicOTP
 {
     public static class Authenticator
     {
-        public static long TimeOffset { get; set; }
+        private const long EPOCH_TICKS = 621355968000000000L;
+        private const long SECOND_TICKS = 10000000L;
 
+        private static long m_TimeOffset = 0;
 
+        private static long CorrectedTicks => DateTime.UtcNow.Ticks - EPOCH_TICKS + m_TimeOffset;
+
+        /// <summary>
+        /// Get an OTP code
+        /// </summary>
         public static string GetCode(OtpKey otpKey)
         {
             if (otpKey.AuthType == AuthTypes.TOTP)
             {
-                TimeSpan ts = (DateTime.UtcNow.AddTicks(TimeOffset) - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc));
-                long challenge = (long)ts.TotalSeconds / otpKey.Period;
-
+                long challenge = CorrectedTicks / (otpKey.Period * SECOND_TICKS);
                 return GetCode(otpKey, (ulong)challenge);
             }
             else
@@ -23,6 +28,94 @@ namespace BasicOTP
                 return GetCode(otpKey, otpKey.Counter);
             }
         }
+
+
+        /// <summary>
+        /// Get remainint time before the current code will change
+        /// </summary>
+        public static TimeSpan GetRemainingTime(OtpKey otpKey)
+        {
+            if (otpKey.AuthType != AuthTypes.TOTP)
+                throw new Exception("Remaining time not supported for non-TOTP authentication");
+
+            long periodTicks = SECOND_TICKS * otpKey.Period;
+            return TimeSpan.FromTicks(periodTicks - (CorrectedTicks % periodTicks) + SECOND_TICKS);
+        }
+
+
+        /// <summary>
+        /// Check if the specified code is valid
+        /// </summary>
+        /// <param name="slidingWindow">How many periods before and after the current time to check</param>
+        public static bool CheckCode(OtpKey otpKey, string code, uint slidingWindow = 0)
+        {
+            if (code == GetCode(otpKey))
+                return true;
+
+            if (slidingWindow == 0)
+                return false;
+
+            if (otpKey.AuthType == AuthTypes.TOTP)
+            {
+                long origOffset = m_TimeOffset;
+                long ticks = TimeSpan.FromSeconds(otpKey.Period * slidingWindow).Ticks;
+
+                m_TimeOffset = origOffset - ticks;
+                bool ret = code == GetCode(otpKey);
+
+                if (!ret)
+                {
+                    m_TimeOffset = origOffset + ticks;
+                    ret = code == GetCode(otpKey);
+                }
+
+                m_TimeOffset = origOffset;
+                return ret;
+            }
+
+            if (otpKey.AuthType == AuthTypes.HOTP)
+            {
+                ulong origCounter = otpKey.Counter;
+
+                otpKey.Counter = origCounter - 1;
+                bool ret = code == GetCode(otpKey);
+
+                if (!ret)
+                {
+                    otpKey.Counter = origCounter + 1;
+                    ret = code == GetCode(otpKey);
+                }
+
+                otpKey.Counter = origCounter;
+                return ret;
+            }
+
+            return false;
+        }
+
+
+        /// <summary>
+        /// If the current computer time is out of sync and generated codes are incorrect, this tries to compensate by checking the current time from google.com
+        /// </summary>
+        public static void SyncTime()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://www.google.com");
+            request.Method = "GET";
+            request.ContentType = "text/html";
+            request.Timeout = 5000;
+
+            using HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new ApplicationException(string.Format("{0}: {1}", (int)response.StatusCode, response.StatusDescription));
+
+            string headerdate = response.Headers["Date"];
+            if (string.IsNullOrEmpty(headerdate) == false)
+                if (DateTime.TryParse(headerdate, out DateTime dt))
+                    m_TimeOffset = (dt - DateTime.Now).Ticks;
+        }
+
+
+
 
         private static string GetCode(OtpKey otpKey, ulong challengeValue)
         {
@@ -60,52 +153,7 @@ namespace BasicOTP
             return code;
         }
 
-        public static bool CheckCode(OtpKey otpKey, string code, uint slidingWindow = 1)
-        {
-            if (code == GetCode(otpKey))
-                return true;
-
-            if (slidingWindow == 0)
-                return false;
-
-            if (otpKey.AuthType == AuthTypes.TOTP)
-            {
-                long origOffset = TimeOffset;
-                long ticks = TimeSpan.FromSeconds(otpKey.Period * slidingWindow).Ticks;
-
-                TimeOffset = origOffset - ticks;
-                bool ret = code == GetCode(otpKey);
-
-                if (!ret)
-                {
-                    TimeOffset = origOffset + ticks;
-                    ret = code == GetCode(otpKey);
-                }
-
-                TimeOffset = origOffset;
-                return ret;
-            }
-            
-            if(otpKey.AuthType == AuthTypes.HOTP)
-            {
-                ulong origCounter = otpKey.Counter;
-
-                otpKey.Counter = origCounter - 1;
-                bool ret = code == GetCode(otpKey);
-
-                if(!ret)
-                {
-                    otpKey.Counter = origCounter + 1;
-                    ret = code == GetCode(otpKey);
-                }
-
-                otpKey.Counter = origCounter;
-                return ret;
-            }
-
-            return false;
-        }
-
+        
         private static HMAC GetHMAC(OtpKey otpKey, byte[] key)
         {
             switch (otpKey.Algorithm)
@@ -125,30 +173,6 @@ namespace BasicOTP
         }
 
 
-        public static void SyncTime()
-        {
-            try
-            {
-                // we use the Header response field from a request to www.google.come
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://www.google.com");
-                request.Method = "GET";
-                request.ContentType = "text/html";
-                request.Timeout = 5000;
-
-                // get response
-                using HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-                // OK?
-                if (response.StatusCode != HttpStatusCode.OK)
-                    throw new ApplicationException(string.Format("{0}: {1}", (int)response.StatusCode, response.StatusDescription));
-
-                string headerdate = response.Headers["Date"];
-                if (string.IsNullOrEmpty(headerdate) == false)
-                    if (DateTime.TryParse(headerdate, out DateTime dt))
-                        TimeOffset = (dt - DateTime.Now).Ticks;
-            }
-            catch { }
-        }
-
+        
     }
 }
